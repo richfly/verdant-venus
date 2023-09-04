4c5e49e559734c8e39fb5a6001d5a04a16cf40b6
richfly <32109737+richfly@users.noreply.github.com>
2023-09-04 12:58:05 -0700

    tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      backgroundColor: "#0B263B",
+      backgroundColor: {
+        "custom-blue": "#0B263B",
+      },
     },
   },
   plugins: [],
